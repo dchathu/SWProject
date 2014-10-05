ddb0e7fda9900515dd3ff40799e63bedf459fa65
dchathu30 <dchathu30@gmail.com>
2014-10-05 14:56:34 -0700

    Login enabled

--- Software/Program.cs
+++ Software/Program.cs
@@ -16,7 +16,7 @@ namespace Software
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CoverPage());
+            Application.Run(new LoginTemp());
         }
     }
 }
